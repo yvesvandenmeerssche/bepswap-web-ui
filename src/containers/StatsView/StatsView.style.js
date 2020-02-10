05ea174f6319dbc4326046459bbe7daf97c84733
Veado <5270325-veado@users.noreply.gitlab.com>
2020-02-10 17:26:44 +0000

    Fix `StatsView` component
    - Migrate to TypeScript
    - Add `Connect Wallet` button
    - Fix handling of user

--- src/containers/StatsView/StatsView.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-import ContentView from '../../components/utility/contentView';
-
-export const ContentWrapper = styled(ContentView)`
-  & > .ant-row {
-    display: flex;
-  }
-`;
