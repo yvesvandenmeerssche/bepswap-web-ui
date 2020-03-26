3b66072a5975a5cd0b47c0949b18c338166470c8
cy <luxtars.dev@gmail.com>
2020-03-26 10:37:30 +0000

    Resolve "ADD: Transaction Page"

--- src/components/transaction/txStatus/txStatus.tsx
+++ src/components/transaction/txStatus/txStatus.tsx
@@ -7,35 +7,40 @@ import {
   formatBaseAsTokenAmount,
   baseAmount,
 } from '../../../helpers/tokenHelper';
+import { TESTNET_TX_BASE_URL } from '../../../helpers/apiHelper';
 
 export type RoundValue = 'left' | 'right';
 
 export type Props = {
   type: string;
   data: Coin[];
+  txID?: string;
   round: RoundValue;
 };
 
 const TxStatus: React.FC<Props> = (props: Props): JSX.Element => {
-  const { type, data, round } = props;
+  const { type, data, round, txID } = props;
+  const txURL = txID ? TESTNET_TX_BASE_URL + txID : '#';
 
   return (
-    <TxStatusWrapper className="txStatus-wrapper" round={round}>
-      <p className="txStatus-type">{type}</p>
-      {data.map((txDetail: Coin, index) => {
-        const { asset, amount } = txDetail;
-        const { ticker: assetValue } = getAssetFromString(asset);
-        const amountValue = formatBaseAsTokenAmount(baseAmount(amount));
+    <a href={txURL} target="_blank" rel="noopener noreferrer">
+      <TxStatusWrapper className="txStatus-wrapper" round={round}>
+        <p className="txStatus-type">{type}</p>
+        {data.map((txDetail: Coin, index) => {
+          const { asset, amount } = txDetail;
+          const { ticker: assetValue } = getAssetFromString(asset);
+          const amountValue = formatBaseAsTokenAmount(baseAmount(amount));
 
-        return (
-          <TxStatusContent className="tx-status-content" key={index}>
-            <p className="txStatus-amount">{amountValue}</p>
-            <p className="txStatus-asset">{assetValue}</p>
-            {index < data.length - 1 ? <Seperator /> : ''}
-          </TxStatusContent>
-        );
-      })}
-    </TxStatusWrapper>
+          return (
+            <TxStatusContent className="tx-status-content" key={index}>
+              <p className="txStatus-amount">{amountValue}</p>
+              <p className="txStatus-asset">{assetValue}</p>
+              {index < data.length - 1 ? <Seperator /> : ''}
+            </TxStatusContent>
+          );
+        })}
+      </TxStatusWrapper>
+    </a>
   );
 };
 
