3a66e98fe6c01914d9f1a85119a2007c92b6396f
Chris <luxtars.dev@gmail.com>
2020-03-28 09:31:24 -0400

    Update filter dropdown component

--- src/containers/TransactionView/TransactionView.tsx
+++ src/containers/TransactionView/TransactionView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import * as RD from '@devexperts/remote-data-ts';
