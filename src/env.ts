e81c9601d105ce9c0d3e96052bad5e0de814544a
Veado <veado@protonmail.com>
2020-04-09 19:33:22 +0200

    Use Byzantine to get Midgards base url

--- src/env.ts
+++ src/env.ts
@@ -6,8 +6,21 @@ const dev_hostnames = ['localhost'];
 const hostname = window.location.hostname;
 
 const isMainnet = prod_hostnames.includes(hostname);
-const isTestnet = hostname.includes('testnet');
+// const isTestnet = hostname.includes('testnet');
+const isTestnet = true;
 const isDevnet = dev_hostnames.includes(hostname);
+
+export enum NET {
+  DEV = 'devnet',
+  TEST = 'testnet',
+  MAIN = 'mainnet'
+}
+
+export const getNet = (): NET => {
+  if (isMainnet) return NET.MAIN;
+  if (isTestnet) return NET.TEST;
+  return NET.DEV;
+};
 
 const BINANCE_NET = isMainnet ? binance.Network.MAINNET : binance.Network.TESTNET;
 
