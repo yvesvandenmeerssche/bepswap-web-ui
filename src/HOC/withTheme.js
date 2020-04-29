418148cd45b85b85cb98aa423e00f9da11eadd9e
Veado <veado@protonmail.com>
2020-04-29 17:00:52 +0200

    Remove deprecated stuff in `HOC`

--- src/HOC/withTheme.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from '../settings';
-
-export default component => (
-  <ThemeProvider theme={defaultTheme}>{component}</ThemeProvider>
-);
