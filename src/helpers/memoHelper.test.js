1cfe4eee09bbb2109541b0acf91b0dd32cb60a48
Chris Yu <luxtars.dev@gmail.com>
2020-05-15 13:33:42 -0400

    Update memo

--- src/helpers/memoHelper.test.js
+++ src/helpers/memoHelper.test.js
@@ -9,28 +9,28 @@ describe('helpers/memoHelper/', () => {
   describe('getSwapMemo', () => {
     it('should create a memo for the swap tx', () => {
       const result = getSwapMemo('RUNE', 'xyz', '2');
-      expect(result).toEqual('SWAP:RUNE:xyz:2');
+      expect(result).toEqual('SWAP:BNB.RUNE:xyz:2');
     });
   });
 
   describe('getStakeMemo', () => {
     it('should create a memo for the stake tx', () => {
       const result = getStakeMemo('BNB');
-      expect(result).toEqual('STAKE:BNB');
+      expect(result).toEqual('STAKE:BNB.BNB');
     });
   });
 
   describe('getCreateMemo', () => {
     it('should create a memo for the creating pool', () => {
       const result = getCreateMemo('TUSDB');
-      expect(result).toEqual('STAKE:TUSDB');
+      expect(result).toEqual('STAKE:BNB.TUSDB');
     });
   });
 
   describe('getWithdrawMemo', () => {
     it('should create a memo for the unstake/withdraw tx', () => {
       const result = getWithdrawMemo('FSN', 50);
-      expect(result).toEqual('WITHDRAW:FSN:50');
+      expect(result).toEqual('WITHDRAW:BNB.FSN:50');
     });
   });
 });
