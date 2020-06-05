7c808741593b49a12353f52514ca51ef2a23f2aa
Chris Yu <luxtars.dev@gmail.com>
2020-06-05 13:11:58 -0400

    Add hint for pending status on the pool list

--- src/components/poolFilter/poolFilter.tsx
+++ src/components/poolFilter/poolFilter.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useCallback } from 'react';
-import { CheckCircleOutlined, FieldTimeOutlined } from '@ant-design/icons';
+import { Popover } from 'antd';
+import {
+  CheckCircleOutlined,
+  FieldTimeOutlined,
+} from '@ant-design/icons';
 
-import { PoolFilterWrapper } from './poolFilter.style';
+import { PoolFilterWrapper, PopoverContent, PopoverIcon } from './poolFilter.style';
 import Button from '../uielements/button';
 import { PoolDetailStatusEnum } from '../../types/generated/midgard/api';
 
@@ -18,6 +23,21 @@ const PoolFilter: React.FC<Props> = (props: Props): JSX.Element => {
       onClick(key);
     },
     [onClick],
+  );
+
+  const getPopupContainer = () => {
+    return document.getElementsByClassName('pool-filter')[0] as HTMLElement;
+  };
+
+  const renderPopoverContent = () => (
+    <PopoverContent>
+      Pools don't immediately become enabled on THORChain and new pools must
+      participate in a liquidity competition which is held every 50000 blocks
+      (approximately 3 days). The pool with the most liquidity measured in RUNE
+      wins and becomes enabled. During the competition phase, liquidity can be
+      added & withdrawn from the pending pools, however users cannot swap across
+      these pools.
+    </PopoverContent>
   );
 
   return (
@@ -40,6 +60,19 @@ const PoolFilter: React.FC<Props> = (props: Props): JSX.Element => {
         <FieldTimeOutlined />
         Pending
       </Button>
+      <Popover
+        content={renderPopoverContent}
+        getPopupContainer={getPopupContainer}
+        placement="bottomRight"
+        overlayClassName="pool-filter-info"
+        overlayStyle={{
+          padding: '6px',
+          animationDuration: '0s !important',
+          animation: 'none !important',
+        }}
+      >
+        <PopoverIcon />
+      </Popover>
     </PoolFilterWrapper>
   );
 };
