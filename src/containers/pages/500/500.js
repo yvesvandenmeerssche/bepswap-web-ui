6b558e40dead6ac93122765cd4db73c2aae28688
Chris Yu <luxtars.dev@gmail.com>
2020-06-08 10:24:47 -0400

    Add maintenance page by app env

--- src/containers/pages/500/500.js
+++ src/containers/pages/500/500.js
@@ -1,11 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { Component } from 'react';
+
+import Label from '../../../components/uielements/label';
+import Logo from '../../../components/uielements/logo';
+import { AppLayout } from './style';
 
 class Page500 extends Component {
   render() {
     return (
-      <div>
-        <div>500 Page</div>
-      </div>
+      <AppLayout>
+        <Logo name="bepswap" type="long" />
+        <Label size="large">Thanks for trying out testnet!</Label>
+        <Label size="big">
+          we've learned a lot and will be relaunching with new changes from
+          community feedback.
+        </Label>
+      </AppLayout>
     );
   }
 }
