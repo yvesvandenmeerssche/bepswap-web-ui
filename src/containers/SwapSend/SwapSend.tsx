e9222218530ff1c3df1875f38fb555b4fa3a5925
Chris Yu <luxtars.dev@gmail.com>
2020-08-17 00:02:39 -0400

    Fix swap route issue

--- src/containers/SwapSend/SwapSend.tsx
+++ src/containers/SwapSend/SwapSend.tsx
@@ -544,8 +544,8 @@ const SwapSend: React.FC<Props> = (props: Props): JSX.Element => {
     if (sourceSymbol && targetSymbol) {
       const URL =
         source === selectedToken
-          ? `/swap/${targetSymbol}-${sourceSymbol}`
-          : `/swap/${sourceSymbol}-${targetSymbol}`;
+          ? `/swap/${targetSymbol}:${sourceSymbol}`
+          : `/swap/${sourceSymbol}:${targetSymbol}`;
       history.push(URL);
     } else {
       // eslint-disable-next-line no-console
@@ -558,7 +558,7 @@ const SwapSend: React.FC<Props> = (props: Props): JSX.Element => {
   const handleReversePair = () => {
     if (sourceSymbol && targetSymbol) {
       setXValue(tokenAmount(0));
-      const URL = `/swap/${targetSymbol}-${sourceSymbol}`;
+      const URL = `/swap/${targetSymbol}:${sourceSymbol}`;
       history.push(URL);
     } else {
       // eslint-disable-next-line no-console
