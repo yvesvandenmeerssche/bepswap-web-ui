e9e3d091174c9acbd97ef8588a2692f0b24f85c6
FrozenIce0617 <prosdev087>
2020-08-31 13:38:33 +0100

    Fix the password confirmation modal lock icon

--- src/components/modals/privateModal/privateModal.style.ts
+++ src/components/modals/privateModal/privateModal.style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { palette } from 'styled-theme';
 import Modal from '../../uielements/modal';
 
 export const StyledModal = styled(Modal)`
@@ -13,4 +14,12 @@ export const ModalContent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+export const ModalIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  color: ${palette('text', 2)};
 `;
