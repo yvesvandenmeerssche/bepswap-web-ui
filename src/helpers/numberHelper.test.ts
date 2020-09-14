5a7638b41f11ca45dc8c5b690fc26990ee30b993
FrozenIce0617 <prosdev087>
2020-09-14 11:29:01 +0100

    Update test cases for number helper

--- src/helpers/numberHelper.test.ts
+++ src/helpers/numberHelper.test.ts
@@ -4,22 +4,22 @@ describe('helpers/numberHelper/', () => {
   // abbreviateNumber
   describe('abbreviateNumber', () => {
     it('should return short number with K', () => {
-      const result = abbreviateNumber(1857);
+      const result = abbreviateNumber(1857, 3);
       expect(result).toEqual('1.857 K');
     });
 
     it('should return short number with M', () => {
-      const result = abbreviateNumber(10938923);
+      const result = abbreviateNumber(10938923, 3);
       expect(result).toEqual('10.939 M');
     });
 
     it('should return short number with B', () => {
-      const result = abbreviateNumber(4107932892);
+      const result = abbreviateNumber(4107932892, 3);
       expect(result).toEqual('4.108 B');
     });
 
     it('should return short number with t', () => {
-      const result = abbreviateNumber(84310979328923);
+      const result = abbreviateNumber(84310979328923, 3);
       expect(result).toEqual('84.311 t');
     });
   });
