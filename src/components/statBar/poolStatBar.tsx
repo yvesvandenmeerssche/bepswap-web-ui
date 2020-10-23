ce6eaf3540775ba91b9c112f93c2c68f62e8cfa4
luxtardev <luxtars.dev@gmail.com>
2020-10-23 16:07:36 -0400

    Refactor pool stats bar for detail page

--- src/components/statBar/poolStatBar.tsx
+++ src/components/statBar/poolStatBar.tsx
@@ -35,138 +35,76 @@ const Statistics: React.FC<Props> = (props: Props): JSX.Element => {
   const totalSwaps = `${poolInfo?.swappingTxCount ?? '0'}`;
 
   const poolAPY = bnOrZero(poolInfo?.poolAPY);
-  const poolAPYLabel = poolAPY.multipliedBy(100).toFixed(2);
+  const poolAPYLabel = `${poolAPY.multipliedBy(100).toFixed(2)} %`;
+
+  const poolStats = React.useMemo(
+    () => [
+      {
+        title: 'Total Liquidity',
+        value: liquidityValue,
+      },
+      {
+        title: 'Total Earnings',
+        value: earning,
+      },
+      {
+        title: 'Total Volume',
+        value: volume,
+      },
+      {
+        title: '24H Volume',
+        value: volume24,
+      },
+      {
+        title: 'Total Members',
+        value: totalStakers,
+      },
+      {
+        title: 'Total Swaps',
+        value: totalSwaps,
+      },
+      {
+        title: 'Total Transactions',
+        value: transaction,
+      },
+      {
+        title: 'APY',
+        value: poolAPYLabel,
+      },
+    ],
+    [
+      liquidityValue,
+      earning,
+      volume,
+      volume24,
+      totalStakers,
+      totalSwaps,
+      transaction,
+      poolAPYLabel,
+    ],
+  );
 
   return (
     <>
       <Row gutter={[16, 16]}>
-        <Col
-          xs={{ span: 24 }}
-          sm={{ span: 24 }}
-          md={{ span: 12 }}
-          lg={{ span: 12 }}
-          xl={{ span: 12 }}
-        >
-          <StyledStatistic
-            title="Total Liquidity"
-            formatter={() => {
-              if (loading) return <LabelLoader />;
-              return <span>{liquidityValue}</span>;
-            }}
-          />
-        </Col>
-        <Col
-          xs={{ span: 24 }}
-          sm={{ span: 24 }}
-          md={{ span: 12 }}
-          lg={{ span: 12 }}
-          xl={{ span: 12 }}
-        >
-          <StyledStatistic
-            title="Total Earnings"
-            formatter={() => {
-              if (loading) return <LabelLoader />;
-              return <span>{earning}</span>;
-            }}
-          />
-        </Col>
-      </Row>
-      <Row gutter={[16, 16]}>
-        <Col
-          xs={{ span: 24 }}
-          sm={{ span: 24 }}
-          md={{ span: 12 }}
-          lg={{ span: 12 }}
-          xl={{ span: 12 }}
-        >
-          <StyledStatistic
-            title="Total Volume"
-            formatter={() => {
-              if (loading) return <LabelLoader />;
-              return <span>{volume}</span>;
-            }}
-          />
-        </Col>
-        <Col
-          xs={{ span: 24 }}
-          sm={{ span: 24 }}
-          md={{ span: 12 }}
-          lg={{ span: 12 }}
-          xl={{ span: 12 }}
-        >
-          <StyledStatistic
-            title="24H Volume"
-            formatter={() => {
-              if (loading) return <LabelLoader />;
-              return <span>{volume24}</span>;
-            }}
-          />
-        </Col>
-      </Row>
-      <Row gutter={[16, 16]}>
-        <Col
-          xs={{ span: 24 }}
-          sm={{ span: 24 }}
-          md={{ span: 12 }}
-          lg={{ span: 12 }}
-          xl={{ span: 12 }}
-        >
-          <StyledStatistic
-            title="Total Members"
-            formatter={() => {
-              if (loading) return <LabelLoader />;
-              return <span>{totalStakers}</span>;
-            }}
-          />
-        </Col>
-        <Col
-          xs={{ span: 24 }}
-          sm={{ span: 24 }}
-          md={{ span: 12 }}
-          lg={{ span: 12 }}
-          xl={{ span: 12 }}
-        >
-          <StyledStatistic
-            title="Total Swaps"
-            formatter={() => {
-              if (loading) return <LabelLoader />;
-              return <span>{totalSwaps}</span>;
-            }}
-          />
-        </Col>
-      </Row>
-      <Row gutter={[16, 16]}>
-        <Col
-          xs={{ span: 24 }}
-          sm={{ span: 24 }}
-          md={{ span: 12 }}
-          lg={{ span: 12 }}
-          xl={{ span: 12 }}
-        >
-          <StyledStatistic
-            title="Total Transactions"
-            formatter={() => {
-              if (loading) return <LabelLoader />;
-              return <span>{transaction}</span>;
-            }}
-          />
-        </Col>
-        <Col
-          xs={{ span: 24 }}
-          sm={{ span: 24 }}
-          md={{ span: 12 }}
-          lg={{ span: 12 }}
-          xl={{ span: 12 }}
-        >
-          <StyledStatistic
-            title="APY"
-            formatter={() => {
-              if (loading) return <LabelLoader />;
-              return <span>{poolAPYLabel}</span>;
-            }}
-            suffix={loading ? '' : '%'}
-          />
-        </Col>
+        {poolStats.map((statsProp, index) => (
+          <Col
+            key={index}
+            xs={{ span: 24 }}
+            sm={{ span: 24 }}
+            md={{ span: 12 }}
+            lg={{ span: 12 }}
+            xl={{ span: 12 }}
+          >
+            <StyledStatistic
+              title={statsProp.title}
+              formatter={() => {
+                if (loading) return <LabelLoader />;
+                return <span>{statsProp.value}</span>;
+              }}
+            />
+          </Col>
+        ))}
       </Row>
     </>
   );
